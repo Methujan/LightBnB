2e86bb16c0f6b9e7b11356874eda4d5d16770360
Methujan Jeyapalan <methujan_02@hotmail.com>
2021-06-24 22:32:09 +0000

    Create new property

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -162,7 +162,7 @@ exports.getAllReservations = getAllReservations;
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
-  LEFT JOIN property_reviews ON properties.id = property_id
+  JOIN property_reviews ON properties.id = property_id
   `;
 
   if (options.city) {
@@ -240,9 +240,32 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
+  queryString = `
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url,  cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, active, province, city, country, street, post_code)
+  VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *; 
+  `;
+  return pool
+  .query(queryString,[property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code])
+
+  .then((result) => {
+    if(result.rows){
+      console.log(result.rows);
+      return result.rows[0];
+    } else {
+      return null;
+    }
+  })
+
+  .catch((err) => {
+    console.log(err.message);
+    return err.message;
+  });
+/*
   const propertyId = Object.keys(properties).length + 1;
   property.id = propertyId;
   properties[propertyId] = property;
   return Promise.resolve(property);
+  */
 }
 exports.addProperty = addProperty;
